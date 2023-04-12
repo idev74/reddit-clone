8e38fd6dffe2e8e89eaae6c12c7f408ab43f7dd1
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-11 22:35:28 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+const app = express();
+
+const hbs = handlebars.create({
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        foo() { return 'FOO!'; },
+        bar() { return 'BAR!'; }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000);
