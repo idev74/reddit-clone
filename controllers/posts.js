4bd98392c4e9c1fb1c3fe02ad8383713396ca6da
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-17 19:21:48 -0700

    usets can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,46 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
-    app.post('/posts/new', (req, res) => {
-      const post = new Post(req.body);
+  app.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find({}).lean();
+      return res.render('posts-index', { posts });
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
 
-      post.save(() => res.redirect('/'));
-    });
-  };
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+  });
+
+  app.post('/posts/new', async (req, res) => {
+    const post = new Post(req.body);
+  
+    try {
+      await post.save();
+      res.redirect('/');
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  app.get('/posts/:id', async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id).lean().populate('comments')
+      .then((post) => res.render('posts-show', { post }))
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  app.get('/n/:subreddit', async (req, res) => {
+    try {
+      const posts = await Post.find({ subreddit: req.params.subreddit }).lean();
+      res.render('posts-index', { posts });
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+
+app.engine('handlebars', hbs.engine({ defaultLayout: 'main', partialsDir: __dirname + '/views/partials'}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+require('./controllers/posts')(app);
+require('./data/reddit-db');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+});
+
+app.listen(3000);
+
+module.exports = app;
