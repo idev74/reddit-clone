d2973b235ed7b49b873133f542ec7b29bb4b4454
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-17 19:21:46 -0700

    users can see individual posts

--- models/post.js
+++ models/post.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose');
 const postSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
-  summary: { type: String, required: true }
+  summary: { type: String, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 });
 
 module.exports = model('Post', postSchema);
