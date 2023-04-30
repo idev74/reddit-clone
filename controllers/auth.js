94d06a71c443d0f68748a92c8dacd1274d5d2917
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-30 15:07:38 -0700

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,4 +21,30 @@ module.exports = (app) => {
         res.clearCookie('nToken');
         return res.redirect('/');
     });
+
+    app.get('/login', (req, res) => res.render('login'));
+
+    app.post('/login', (req, res) => {
+        const { username, password } = req.body;
+        User.findOne({ username }, 'username password')
+            .then((user) => {
+                if (!user) {
+                    return res.status(401).send({ message: 'Wrong Username or Password' });
+                }
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                        return res.status(401).send({ message: 'Wrong Username or password' });
+                    }
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                        expiresIn: '60 days',
+                    });
+                    res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+                    return res.redirect('/');
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+
 };
