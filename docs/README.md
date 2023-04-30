72846b831411a6b83c61502ca8b2ea2eacdf277f
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-04-30 10:22:45 -0700

    added docsify

---
+++ docs/README.md
@@ -0,0 +1,4 @@
+# Reddit JS
+
+> A Reddit clone made with Node JS, Express, and Handlebars!
+
--- test/posts.js
+++ test/posts.js
@@ -1,4 +1,3 @@
-// test/posts.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { describe, it } = require('mocha');
@@ -6,14 +5,11 @@ chai.use(chaiHttp);
 const app = require('../server');
 const agent = chai.request.agent(app);
 
-// Import the Post model from our models folder so we
-// we can use it in our tests.
 const Post = require('../models/post');
 
 const should = chai.should();
 
 describe('Posts', () => {
-    // Post that we'll use for testing purposes
     const newPost = {
         title: 'post title',
         url: 'https://www.google.com',
