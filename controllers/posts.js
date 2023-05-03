c5c1e42ac51874abf20d9aa6a301f5c238516e71
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-05-03 13:26:42 -0700

    Authors for posts are available

--- controllers/posts.js
+++ controllers/posts.js
@@ -16,36 +16,43 @@ module.exports = (app) => {
   });
 
   app.post('/posts/new', async (req, res) => {
-    try {
-        if (req.user) {
-            const post = new Post(req.body);
-            await post.save();
-            return res.redirect('/');
-        } else {
-            return res.status(401).send();
-        }
-    } catch (err) {
+    if (req.user) {
+      const userId = req.user._id;
+      const post = new Post(req.body);
+      post.author = userId;
+  
+      try {
+        await post.save();
+        const user = await User.findById(userId);
+        user.posts.unshift(post);
+        await user.save();
+        return res.redirect(`/posts/${post._id}`);
+      } catch (err) {
         console.log(err.message);
-        return res.status(400).send({ err });
-    }
-});
-
-  app.get('/posts/:id', async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id).lean().populate('comments')
-        .then((post) => res.render('posts-show', { post }))
-    } catch (err) {
-      console.log(err.message);
+      }
+    } else {
+      return res.status(401); 
     }
   });
 
-  app.get('/n/:subreddit', async (req, res) => {
-    try {
-      const posts = await Post.find({ subreddit: req.params.subreddit }).lean();
-      res.render('posts-index', { posts });
-    } catch (err) {
-      console.log(err.message);
-    }
+  app.get('/posts/:id', (req, res) => {
+    const currentUser = req.user;
+  
+    Post.findById(req.params.id).lean().populate('comments').populate('author')
+      .then((post) => res.render('posts-show', { post, currentUser }))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+  
+  app.get('/n/:subreddit', (req, res) => {
+    const currentUser = req.user;
+    const { subreddit } = req.params;
+    Post.find({ subreddit }).lean().populate('author')
+      .then((posts) => res.render('posts-index', { posts, currentUser }))
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
   app.get('/', (req, res) => {
